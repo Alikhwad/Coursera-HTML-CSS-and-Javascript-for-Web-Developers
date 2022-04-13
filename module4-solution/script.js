58e5d411fa7073984e806d1caea1338f4f354a8e
Ali <alikhwad@gmail.com>
2022-04-13 10:37:47 +0200

    module 4

---
+++ module4-solution/script.js
@@ -0,0 +1,10 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+  if (firstLetter === "j") {
+    console.log(byeSpeaker.speak(names[i]));
+  }
+  else {
+    console.log(helloSpeaker.speak(names[i]));
+  }
+}
